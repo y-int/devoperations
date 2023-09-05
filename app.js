b6893f1185b213c689d3fb65651c5cf94d94ca8a
y62 <y62@outlook.dk>
2023-09-05 17:46:42 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Simple Express HTML</title>
+        </head>
+        <body>
+            <h1>Welcome to my Express App</h1>
+            <p>This is a simple webpage served from an Express app.</p>
+        </body>
+        </html>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
